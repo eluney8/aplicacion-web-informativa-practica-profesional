12b01d8a91ea0552765e231eeda6432ee7dd55d8
N1liaq <ailinyanos83@gmail.com>
2026-06-10 14:33:53 -0300

    se actualizó la base en <registro>

---
+++ assets/js/app_registro.js
@@ -0,0 +1,8 @@
+const nombreRegist = document.getElementById("nombreRegist");
+const apellidoRegist = document.getElementById("apellidoRegist");
+const cuilRegist = document.getElementById("cuilRegist");
+const emailRegist = document.getElementById("emailRegist");
+const contraseñaRegist = document.getElementById("contraseñaRegist");
+const contraseñaVerificacionRegist = document.getElementById(
+  "contraseñaVerificacionRegist",
+);
